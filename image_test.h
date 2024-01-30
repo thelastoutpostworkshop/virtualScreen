fd150b8ce065d1d0caabb014153e7b7c836be651
charles <cgiguere@iname.com>
2024-01-29 19:21:39 -0500

    image_test

--- image_test.h
+++ image_test.h
@@ -2,12 +2,20 @@
 #include "images/newyork_960x240.h"
 #include "images/highway_720x480.h"
 #include "images/night_earth_960x240.h"
+#include "images/bazar_960x240.h"
+#include "images/spacebattle_960x240.h"
+
+// #define test_image bazar
+// #define test_width bazar_width
+// #define test_height bazar_height
+
+#define test_image spacebattle
+#define test_width spacebattle_width
+#define test_height spacebattle_height
 
 void image_test(VirtualDisplay *tft)
 {
-    tft->drawRGBBitmap(0, 0, (uint16_t *)newyork, newyork_width, newyork_height);
-    tft->output();
-    delay(5000);
-    tft->drawRGBBitmap(0, 0, (uint16_t *)night_earth, night_earth_width, night_earth_height);
+    tft->fillScreen(TFT_BLACK);
+    tft->drawRGBBitmap(0, 0, (uint16_t *)test_image, test_width, test_height);
     tft->output();
 }
