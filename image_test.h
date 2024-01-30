87625eb76d044b2a3cc11d9cc2f0630f71f91bcb
charles <cgiguere@iname.com>
2024-01-29 19:29:43 -0500

    image_test

--- image_test.h
+++ image_test.h
@@ -4,14 +4,19 @@
 #include "images/night_earth_960x240.h"
 #include "images/bazar_960x240.h"
 #include "images/spacebattle_960x240.h"
+#include "images/console_960x240.h"
 
 // #define test_image bazar
 // #define test_width bazar_width
 // #define test_height bazar_height
 
-#define test_image spacebattle
-#define test_width spacebattle_width
-#define test_height spacebattle_height
+// #define test_image spacebattle
+// #define test_width spacebattle_width
+// #define test_height spacebattle_height
+
+#define test_image console
+#define test_width console_width
+#define test_height console_height
 
 void image_test(VirtualDisplay *tft)
 {
