901e843c106afba7008a326b7aeeea75f67bbe41
charles <cgiguere@iname.com>
2024-02-03 13:37:44 -0500

    clocks

--- virtualScreen.ino
+++ virtualScreen.ino
@@ -1,5 +1,7 @@
 #include "virtualScreen.h"
 #include "images/clock_1.h"
+#include "images/clock_2.h"
+#include "images/clock_3.h"
 
 VirtualDisplay *tft;
 
@@ -12,7 +14,7 @@ void setup()
     // 6 screens 3 on each row
     screens.addRow({{7, 2}, {6, 2}, {15, 2}});
     screens.addRow({{16, 0}, {11, 0}, {9, 0}});
-    
+
     tft = new VirtualDisplay(screens.width(), screens.height(), &screens);
 
     if (!tft->begin())
@@ -23,6 +25,8 @@ void setup()
 
     tft->fillScreen(TFT_BLACK);
     tft->pushImage(0, 0, clock1_width, clock1_height, (uint16_t *)clock1);
+    tft->pushImage(240, 0, clock2_width, clock2_height, (uint16_t *)clock2);
+    tft->pushImage(480, 0, clock3_width, clock3_height, (uint16_t *)clock3);
 
     tft->output();
 }
